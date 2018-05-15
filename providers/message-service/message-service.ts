94f3d72491f44d6eb6762f6c0ca36b798d49d836
qomtec <qomtecsol@gmail.com>
2018-05-15 12:02:58 -0600

    15_05_2018

--- providers/message-service/message-service.ts
+++ providers/message-service/message-service.ts
@@ -9,22 +9,47 @@ import { Mensaje } from '../../models/mensaje.model';
 @Injectable()
 export class MessageServiceProvider {
 
-  constructor(public db: AngularFireDatabase) {  }
-  public createMessage(mensaje: Mensaje,lista: AngularFireList<Mensaje>): Promise<any>{
-    
+  constructor(public db: AngularFireDatabase) { }
+  public createMessage(mensaje: Mensaje, lista: AngularFireList<Mensaje>): Promise<any> {
+
     mensaje.timestamp = firebase.database.ServerValue.TIMESTAMP;
     console.log(mensaje);
     return Promise.resolve(lista.push(mensaje));
   }
-  public listMessajes(mensaje: Mensaje): AngularFireList<Mensaje>{
-    return this.db.list('/citas/'+ mensaje.para + "-"+ mensaje.de,
-    ref => ref.limitToLast(10).orderByChild('timestamp'));
+  public listMessajes(mensaje: Mensaje): AngularFireList<Mensaje> {
+    return this.db.list('/citas/' + mensaje.para + "-" + mensaje.de,
+      ref => ref.limitToLast(10).orderByChild('timestamp'));
   }
-  public listCitas(codigo: string): Promise<Mensaje>{
+
+  public listCitas(codigo: string): Promise<Mensaje> {
+    return new Promise((resolve, reject) => {
+      let query = firebase.database().ref('/citas').orderByKey();
+      query.once("value", (data) => {
+        let d = data.val();
+        let datos = [];
+        for (var key in d) {
+          datos.push(d[key]);
+        }
+        console.log(datos);
+        
+      })
+    })
+    /*
+    let query = firebase.database().ref('/citas').orderByKey();
+    query.once("value")
+    .then(data =>{
+      data.forEach( d=> {
+        let key = d.key;
+        let child = d.val();
+        console.log(child);
+
+      });
+    }).catch();
+    --------------------------
     return new Promise((resolve, reject) =>{
       let itemsRef: AngularFireList<Mensaje>;
       let items: Observable<Mensaje[]>;
-      itemsRef = this.db.list('/citas', ref => ref.orderByKey().orderByChild('de').equalTo(codigo));
+      itemsRef = this.db.list('/citas', ref => ref.orderByChild('de').equalTo(codigo));
       items = itemsRef.valueChanges();
       items.forEach(i => {
         let obj: any = [];
@@ -46,7 +71,7 @@ export class MessageServiceProvider {
       .catch ((err: any) => {
         reject(err);
       })
-    });
+    });*/
   }
 
 }
